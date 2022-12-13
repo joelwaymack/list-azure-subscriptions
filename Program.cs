02c6d9a84e9be440f8939665f42f72d095c729d3
Joel Waymack <joel.waymack@microsoft.com>
2022-12-13 12:50:30 -0600

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Identity;
+using Azure.ResourceManager;
+
+Console.WriteLine("*** List Tenants and Subscriptions ***");
+
+var credential = new DefaultAzureCredential(new DefaultAzureCredentialOptions());
+var arm = new ArmClient(credential);
+
+var tenants = arm.GetTenants();
+foreach (var tenant in tenants)
+{
+    try
+    {
+        var tenantCred = new DefaultAzureCredential(new DefaultAzureCredentialOptions { TenantId = tenant.Data.TenantId.ToString() });
+        var tenantArm = new ArmClient(tenantCred);
+
+        Console.WriteLine($"{tenant.Data.DisplayName} - {tenant.Data.TenantId.ToString()}");
+        var subscriptions = tenantArm.GetSubscriptions();
+
+        foreach (var subscription in subscriptions)
+        {
+            Console.WriteLine($"\t{subscription.Data.DisplayName}");
+        }
+    }
+    catch
+    {
+        Console.WriteLine($"Error retrieving subscriptions for {tenant.Data.DisplayName}");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# list-azure-subscriptions
-Lists all Azure subscriptions across tenants that a user has access to.
+# List Azure Subscriptions
+
+This app lists all Azure subscriptions across tenants that a user has access to based on the Azure CLI login.
+
+A new ArmClient and DefaultCredential is needed for each queried tenant based on [this GitHub issue](https://github.com/Azure/azure-sdk-for-net/issues/26553).
+
+## Requirements
+
+* [Azure CLI](https://learn.microsoft.com/en-us/cli/azure/install-azure-cli)
+* [.NET 7 SDK](https://dotnet.microsoft.com/en-us/download/dotnet/7.0)
+
+## To Run
+
+In a terminal:
+
+1. Login to the Azure CLI.
+
+    ```bash
+    az login
+    ```
+
+1. Run the app in the same directory as the .csproj file.
+
+    ```bash
+    dotnet run
+    ```
